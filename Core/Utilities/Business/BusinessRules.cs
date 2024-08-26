d61f8523b018b8859d2fb35299046d002e1915cd
esmairemtek <esmairemtek12@gmail.com>
2024-08-26 13:30:49 +0300

    business rule tool

---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        // retuns the rule if a rule fails, returns null if all rules are successful
+        public static IResult Run(params IResult[] rules)
+        {
+            foreach (var rule in rules)
+            {
+                if (!rule.Success)
+                {
+                    return rule;
+                }
+            }
+            return null;
+        }
+    }
+}
