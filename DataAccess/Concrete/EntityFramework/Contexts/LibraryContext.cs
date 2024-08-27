23eb2f6819f0bc5fa26efe02cd64c19bc2a2beed
esmairemtek <esmairemtek12@gmail.com>
2024-08-27 11:19:55 +0300

    update table design

--- DataAccess/Concrete/EntityFramework/Contexts/LibraryContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/LibraryContext.cs
@@ -10,42 +10,57 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
         public DbSet<Genre> Genres { get; set; }
         public DbSet<BookList> BookLists { get; set; }
         public DbSet<User> Users { get; set; }
-        
+        public DbSet<BookGenre> BookGenres { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookListBook> BookListBooks { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Library;Trusted_Connection=true");
         }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<BookAuthor>()
-        //        .HasKey(ba => new { ba.BookId, ba.AuthorId });
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // many-to-many relationship between Book and Genre
+            modelBuilder.Entity<BookGenre>()
+                .HasKey(bg => new { bg.BookId, bg.GenreId });
 
-        //    modelBuilder.Entity<BookAuthor>()
-        //        .HasOne(ba => ba.Book)
-        //        .WithMany(b => b.BookAuthors)
-        //        .HasForeignKey(ba => ba.BookId);
+            modelBuilder.Entity<BookGenre>()
+                .HasOne(bg => bg.Book)
+                .WithMany(b => b.BookGenres)
+                .HasForeignKey(bg => bg.BookId);
+            modelBuilder.Entity<BookGenre>()
+                .HasOne(bg => bg.Genre)
+                .WithMany(g => g.BookGenres)
+                .HasForeignKey(bg => bg.GenreId);
 
-        //    modelBuilder.Entity<BookAuthor>()
-        //        .HasOne(ba => ba.Author)
-        //        .WithMany(a => a.BookAuthors)
-        //        .HasForeignKey(ba => ba.AuthorId);
+            // many-to-many relationship between Book and Author
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+            
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
 
-        //    modelBuilder.Entity<Book>()
-        //        .HasOne(b => b.Genre)
-        //        .WithMany(g => g.Books)
-        //        .HasForeignKey(b => b.GenreId);
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
 
-        //    modelBuilder.Entity<BorrowingRecord>()
-        //        .HasOne(br => br.User)
-        //        .WithMany(u => u.BorrowingRecords)
-        //        .HasForeignKey(br => br.UserId);
+            // many-to-many relationship between BookList and Book
+            modelBuilder.Entity<BookListBook>()
+                .HasKey(blb => new { blb.BookListId, blb.BookId });
 
-        //    modelBuilder.Entity<BorrowingRecord>()
-        //        .HasOne(br => br.Book)
-        //        .WithMany(b => b.BorrowingRecords)
-        //        .HasForeignKey(br => br.BookId);
-        //}
+            modelBuilder.Entity<BookListBook>()
+                .HasOne(blb => blb.BookList)
+                .WithMany(bl => bl.BookListBooks)
+                .HasForeignKey(blb => blb.BookListId);
 
+            modelBuilder.Entity<BookListBook>()
+                .HasOne(blb => blb.Book)
+                .WithMany(b => b.BookListBooks)
+                .HasForeignKey(blb => blb.BookId);
+        }
     }
 }
