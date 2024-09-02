4cb8445cdd3b60cc1ab13617131eb98c9470370a
esmairemtek <esmairemtek12@gmail.com>
2024-09-02 11:54:27 +0300

    added automapper

---
+++ Business/Utilities/AutomapperProfiles/MappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Entity.Concrete;
+using Entity.DTOs.Details;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Utilities.AutomapperProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // author to authordetaildto
+            CreateMap<Author, AuthorDetailDto>();
+
+            // genre to genredetaildto
+            CreateMap<Genre, GenreDetailDto>();
+
+            // book to bookdetaildto
+            CreateMap<Book, BookDetailDto>()
+                .ForMember(dest => dest.Authors, opt => opt.MapFrom(src => src.Authors))
+                .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.Genres));
+
+
+            // booklist to booklistdetaildto
+            CreateMap<BookList, BookListDetailDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
+                .ForMember(dest => dest.Books, opt => opt.MapFrom(src => src.Books));
+
+            //CreateMap<User, UserDetailDto>();
+        }
+    }
+}
