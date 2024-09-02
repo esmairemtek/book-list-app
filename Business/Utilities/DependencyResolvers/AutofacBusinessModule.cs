6e122401d130232f4ee5c593eed146f25dc71559
esmairemtek <esmairemtek12@gmail.com>
2024-09-02 11:54:17 +0300

    relocation

--- Business/Utilities/DependencyResolvers/AutofacBusinessModule.cs
+++ Business/Utilities/DependencyResolvers/AutofacBusinessModule.cs
@@ -1,6 +1,8 @@
 ﻿using Autofac;
+using AutoMapper;
 using Business.Abstract;
 using Business.Concrete;
+using Business.Utilities.AutomapperProfiles;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.EntityFramework.Contexts;
@@ -11,14 +13,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business.DependencyResolvers
+namespace Business.Utilities.DependencyResolvers
 {
     public class AutofacBusinessModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<LibraryContext>().AsSelf().InstancePerLifetimeScope();
-
             builder.RegisterType<BookManager>().As<IBookService>().InstancePerLifetimeScope();
             builder.RegisterType<EfBookDal>().As<IBookDal>().InstancePerLifetimeScope();
 
@@ -28,6 +29,12 @@ namespace Business.DependencyResolvers
             builder.RegisterType<AuthorManager>().As<IAuthorService>().InstancePerLifetimeScope();
             builder.RegisterType<EfAuthorDal>().As<IAuthorDal>().InstancePerLifetimeScope();
 
+            builder.Register(context => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            })).AsSelf().SingleInstance();
+            builder.Register(c => c.Resolve<MapperConfiguration>().CreateMapper(c.Resolve))
+                .As<IMapper>().InstancePerLifetimeScope();
         }
     }
 }
